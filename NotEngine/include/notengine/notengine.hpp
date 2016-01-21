7acfaf707792c3e2a5cb4ae5bb9f76d8efb91450
Romain Graillot <graillot.romain@gmail.com>
2016-01-22 00:33:21 +0100

    Edited shortcut header

--- NotEngine/include/notengine/notengine.hpp
+++ NotEngine/include/notengine/notengine.hpp
@@ -1,6 +1,7 @@
 #include "system/Singleton.hpp"
 
 #include "graphics/Sprite.hpp"
+#include "graphics/SpriteBuffer.hpp"
 #include "graphics/GraphicsBase.hpp"
 #include "graphics/Graphics2D.hpp"
 #include "graphics/Texture2D.hpp"
